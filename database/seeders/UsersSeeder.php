3061186b610e2d59fc130ea95e77038213b75d33
Jorge <luizfonseca830@gmail.com>
2020-10-29 09:47:20 -0500

    correção da seed

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -3,9 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UsersTableSeeder extends Seeder
+class UsersSeeder extends Seeder
 {
     /**
      * Run the database seeds.
